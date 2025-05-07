17510a4bf3ce5f9b837493bc9c9ddf327a733f04
Kewals2PL <129693495+Kewals2PL@users.noreply.github.com>
2025-05-07 18:07:32 +0200

    pyautogui main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import json
-import mouse
 import time
 import keyboard
+import pyautogui
+try:
+    import ctypes
+    ctypes.windll.user32.SetProcessDPIAware()
+except:
+    pass
 
 def load_config(filename):
     with open(filename, "r") as f:
         return json.load(f)
 
-# "x,y" na "x, y"
 def parse_position(pos_str):
     x, y = map(int, pos_str.split(","))
     return x, y
@@ -20,13 +24,6 @@ guess_x, guess_y = parse_position(config["GUESS_BUTTON_POS"])
 continue_x, continue_y = parse_position(config["CONTINUE_BUTTON_POS"])
 err_x, err_y = parse_position(config["ERROR_BUTTON_CLOSE_POS"])
 
-# TEST IF IT WORKS - **currently THE # DON'T WORK**
-#
-# print("MAP_POS:", map_pos)
-# print("PLACE_FLAG_POS:", place_flag_pos)
-# print("GUESS_BUTTON_POS:", guess_button_pos)
-# print("CONTINUE_BUTTON_POS:", continue_button_pos)
-
 print("Be ready!")
 time.sleep(2)
 print(3)
@@ -37,28 +34,18 @@ print(1)
 time.sleep(1)
 print("Press X to end!")
 
-
+def move_and_click(x, y):
+    pyautogui.moveTo(x, y, duration=0.1)
+    time.sleep(0.05)  # to avoid "slide"
+    pyautogui.click()
 
 while True:
     if keyboard.is_pressed('x'):
         print("Program quitted!")
         break
 
-    mouse.move(map_x, map_y, absolute=True, duration=0.1)
-    #time.sleep(2)
-    mouse.move(place_x,place_y, absolute=True, duration=0.1)
-    time.sleep(0.05)
-    mouse.click('left')
-    #time.sleep(2)
-    mouse.move(guess_x,guess_y, absolute=True, duration=0.1)
-    time.sleep(0.05)
-    mouse.click('left')
-    #time.sleep(2)
-    mouse.move(continue_x,continue_y, absolute=True, duration=0.1)
-    time.sleep(0.05)
-    mouse.click('left')
-    #time.sleep(2)
-    mouse.move(err_x, err_y, absolute=True, duration=0.1) # zamyka error - oczywiscie TYLKO jak sie pojawi
-    time.sleep(0.05)
-    mouse.click('left')
-    #time.sleep(2)
+    move_and_click(map_x, map_y)
+    move_and_click(place_x, place_y)
+    move_and_click(guess_x, guess_y)
+    move_and_click(continue_x, continue_y)
+    move_and_click(err_x, err_y)
